e0392837ea45be6d4562558ac6836a16f480d039
Max Willmott <max.willmott@skipthedishes.ca>
2020-11-03 14:27:35 -0700

    selectors: Memoize selectors

--- src/selectors.ts
+++ src/selectors.ts
@@ -1,48 +1,43 @@
 export type Selector<S, A> = (s: S) => A;
 
-export const map = <S, A, B>(
-    aSelector: Selector<S, A>,
-    fn: (a: A) => B
-): Selector<S, B> => state => fn(aSelector(state));
-
-export const ap = <S, A, B>(
-    aSelector: Selector<S, A>,
-    abSelector: Selector<S, (a: A) => B>
-): Selector<S, B> => state => abSelector(state)(aSelector(state));
-
-export const ap2 = <S, A, B, C>(
-    aSelector: Selector<S, A>,
-    bSelector: Selector<S, B>,
-    ab_c: (a: A) => (b:B) => C
-): Selector<S, C> => ap(bSelector, map(aSelector, ab_c))
-
-export const ap3 = <S, A, B, C, D>(
-    aSelector: Selector<S, A>,
-    bSelector: Selector<S, B>,
-    cSelector: Selector<S, C>,
-    abc_d: (a: A) => (b:B) => (c: C) => D
-): Selector<S, D> => {
-    let b = map(aSelector, abc_d);
-    let c = ap(bSelector, b);
-    return ap(cSelector, c);
-}
-
 //extract the result type of the selector
-type SelectorResult<SL> = SL extends Selector<any, infer A> ? A : never;
+export type SelectorResult<SL> = SL extends Selector<any, infer A> ? A : never;
 
 //"map" a collection of selectors to a collection of their results
-type SelectorResults<S> = {
-    [K in keyof S]: SelectorResult<S[K]>
-}
+export type SelectorResults<S> = {
+  [K in keyof S]: SelectorResult<S[K]>;
+};
+
+const arrayShallowEquality = <T>(a1: T[], a2: T[]): boolean =>
+  a1.length === a2.length && a1.every((a1Val, i) => a1Val == a2[i]);
+
+export const memoizeLastResult = <Args extends any[], R>(
+  fn: (...args: Args) => R
+): ((...args: Args) => R) => {
+  let lastArgs: Args = null;
+  let lastResult: R = null;
+
+  const fn2 = (...args: Args): R => {
+    if (!arrayShallowEquality(args, lastArgs)) {
+      lastResult = fn(...args);
+      lastArgs = args;
+    }
+    return lastResult;
+  };
+
+  return fn2;
+};
+
+export const createSelector = <S, A>(
+  fn: (s: S) => A,
+  memoize = memoizeLastResult
+): Selector<S, A> => memoize(fn);
 
 export const createCompoundSelector = <S, A>(
- selectors: [...Selector<S, any>[]],
- fn: (...ss: SelectorResults<typeof selectors>) => A
-): Selector<S, A> => state => {
-    const selections = selectors.map(sl => sl(state));
-    return fn(...selections)
-}
-
-// declare const s1: Selector<"foo", "bar">;
-// declare const s2: Selector<"foo", number>;
-// const foo = createCompoundSelector([s1, s2], (r1, r2) => 1)
+  selectors: [...Selector<S, any>[]],
+  fn: (...ss: SelectorResults<typeof selectors>) => A,
+  memoize = memoizeLastResult
+): Selector<S, A> => memoize((state) => {
+  const selections = selectors.map((sl) => sl(state));
+  return fn(...selections);
+});

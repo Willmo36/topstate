9f8c24b3a89b48742aa5450f786edf22bf203001
Max Willmott <max.willmott@skipthedishes.ca>
2020-11-04 19:16:04 -0700

    react: Initial version of hooks

---
+++ src/react.ts
@@ -0,0 +1,42 @@
+import * as React from "react";
+import { Selector } from "./selectors";
+import { Action, Store } from "./types";
+
+export function createStoreReactFns<S, A extends Action>() {
+  const StoreContext = React.createContext<Store<S, A>>(null);
+
+  const useStore = () => {
+    const store = React.useContext(StoreContext);
+    assertStoreContextValue(store);
+    return store;
+  };
+
+  const useDispatch = () => useStore().dispatch;
+
+  const useSelector = <A>(selector: Selector<S, A>): A => {
+    const store = useStore();
+    const [result, setResult] = React.useState(selector(store.getState()));
+    React.useEffect(() => {
+      const unsubscribe = store.subscribe((state) => {
+        setResult(selector(state));
+      });
+
+      return () => {
+        unsubscribe();
+      };
+    }, [store]);
+
+    return result;
+  };
+
+  return { useStore, useDispatch, useSelector, StoreContext };
+}
+
+const assertStoreContextValue = <S>(value: S | null): S => {
+  if (value === null) {
+    throw new Error(
+      "Found no Store when trying to read from the context provider."
+    );
+  }
+  return value;
+};
---
+++ src/types.ts
@@ -0,0 +1,22 @@
+export type Reducer<S, A extends Action> = (state: S, action: A) => S;
+export type GetState<S> = () => S;
+export type Subscriber<S> = (s: S) => void;
+export type Dispatcher<S, A extends Action> = (action: A | ActionThunk<S, A>) => void;
+export type Action = { type: string };
+export type ActionThunk<S, A extends Action> = (
+  getState: GetState<S>,
+  dispatcher: Dispatcher<S, A>
+) => void;
+
+export type Logger<S, A extends Action> = {
+  logStart: (action: A) => void;
+  logAction: (action: A) => void;
+  logState: (state: S, stage: "prev" | "next") => void;
+  logEnd: () => void;
+};
+
+export type Store<S, A extends Action> = {
+    getState: GetState<S>;
+    dispatch: Dispatcher<S, A>;
+    subscribe: (cb: Subscriber<S>) => () => void;
+}

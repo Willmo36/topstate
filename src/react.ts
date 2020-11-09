14f8d1751228b9eca7e0a1e96a79e215f37f5e7a
Max Willmott <max.willmott@skipthedishes.ca>
2020-11-09 15:52:13 -0700

    tslint: Add and run prettier

--- src/react.ts
+++ src/react.ts
@@ -3,39 +3,37 @@ import { Selector } from "./selectors";
 import { Action, Store } from "./types";
 
 export function createStoreReactFns<S, A extends Action>() {
-  const StoreContext = React.createContext<Store<S, A> | null>(null);
+	const StoreContext = React.createContext<Store<S, A> | null>(null);
 
-  const useStore = () => {
-    const store = React.useContext(StoreContext);
-    return assertStoreContextValue(store);
-  };
+	const useStore = () => {
+		const store = React.useContext(StoreContext);
+		return assertStoreContextValue(store);
+	};
 
-  const useDispatch = () => useStore().dispatch;
+	const useDispatch = () => useStore().dispatch;
 
-  const useSelector = <A>(selector: Selector<S, A>): A => {
-    const store = useStore();
-    const [result, setResult] = React.useState(selector(store.getState()));
-    React.useEffect(() => {
-      const unsubscribe = store.subscribe((state) => {
-        setResult(selector(state));
-      });
+	const useSelector = <A>(selector: Selector<S, A>): A => {
+		const store = useStore();
+		const [result, setResult] = React.useState(selector(store.getState()));
+		React.useEffect(() => {
+			const unsubscribe = store.subscribe((state) => {
+				setResult(selector(state));
+			});
 
-      return () => {
-        unsubscribe();
-      };
-    }, [store]);
+			return () => {
+				unsubscribe();
+			};
+		}, [store]);
 
-    return result;
-  };
+		return result;
+	};
 
-  return { useStore, useDispatch, useSelector, StoreContext };
+	return { useStore, useDispatch, useSelector, StoreContext };
 }
 
 const assertStoreContextValue = <S>(value: S | null): S => {
-  if (value === null) {
-    throw new Error(
-      "Found no Store when trying to read from the context provider."
-    );
-  }
-  return value;
+	if (value === null) {
+		throw new Error("Found no Store when trying to read from the context provider.");
+	}
+	return value;
 };

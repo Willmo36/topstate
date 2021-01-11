e72c161946afe99536148fbdfe4e165f28cbec31
Max Willmott <max.willmott@skipthedishes.ca>
2021-01-11 14:58:26 -0700

    use-action: Initial docs work

--- src/createReactBindings.ts
+++ src/createReactBindings.ts
@@ -11,16 +11,19 @@ import { Action, Store } from "./types";
 /**
  * Create React Context & Hooks for interacting with the Store
  * Capturing the type parameters so they need not be respecified.
- * @example
+ * @example ```
  * //myProj/src/Store.ts
  * export const {useX, useY, useZ} = createReactBindings<MyState, MyAction>();
+ * ```
  */
 export function createReactBindings<S, A extends Action>() {
 	const StoreContext = createContext<Store<S, A> | null>(null);
 
 	/**
 	 * @returns The Store from the React Context
-	 * @example const store = useStore();
+	 * @example ```
+	 * const store = useStore();
+	 * ```
 	 */
 	const useStore = () => {
 		const store = useContext(StoreContext);
@@ -29,16 +32,19 @@ export function createReactBindings<S, A extends Action>() {
 
 	/**
 	 * @returns The Store.dispatch from the React Context
-	 * @example
+	 * @example```
 	 * const dispatch = useDispatch();
 	 * dispatch(myAction|myThunk);
+	 * ```
 	 */
 	const useDispatch = () => useStore().dispatch;
 
 	/**
 	 * Subscribe to the Store and run the selector upon state changes
 	 * @param selector - The selector to run
-	 * @example const foo = useSelector(fooSelector);
+	 * @example ```
+	 * const foo = useSelector(fooSelector);
+	 * ```
 	 */
 	const useSelector = <A>(selector: Selector<S, A>): A => {
 		const store = useStore();
@@ -60,9 +66,12 @@ export function createReactBindings<S, A extends Action>() {
 	 * Create a callback which runs the given action creator and dispatches it's action result
 	 * @param actionCreator - Once given, this function is set (Not registered in useCallback dependencies).
 	 * A function which takes any value and returns an action to be dispatched
-	 * @example
-	 * const setEmailAddress = useActionCreator<string>(email => ({type: "SET_EMAIL", email}));
+	 * @example ```
+	 * const setEmailAddress = useActionCreator<string>(
+	 * 	email => ({type: "SET_EMAIL", email})
+	 * );
 	 * setEmailAddress("emailaddress");
+	 * ```
 	 */
 	const useActionCreator = <B = void>(actionCreator: (b: B) => A) => {
 		const dispatch = useDispatch();
@@ -78,9 +87,10 @@ export function createReactBindings<S, A extends Action>() {
 	 * Create a callback to dispatch the given action
 	 * @param action - Once given, this action is set (Not registered in useCallback dependencies). Action to dispatch.
 	 * @returns React Callback - A callback which will dispatch the given action. Wrapped in useCallback.
-	 * @example
+	 * @example ```
 	 * const clearFilters = useAction({type: "CLEAR_FILTERS"});
 	 * clearFilters();
+	 * ```
 	 */
 	const useAction = (action: A) => {
 		const dispatch = useDispatch();
